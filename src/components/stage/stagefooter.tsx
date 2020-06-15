40f971cf8a4c84a258b05cc4f040f3eaede8bec4
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-06-15 06:55:13 +0530

    Added footer notes for dijkstra algorithm

--- src/components/stage/stagefooter.tsx
+++ src/components/stage/stagefooter.tsx
@@ -6,10 +6,37 @@ interface Props {
 }
 
 const StageFooter: React.FunctionComponent<Props> = (props) => {
+  const getDijkstraDescription = () => {
+    return (
+      <span>
+        Dijkstra's algorithm can be used for finding <b>shortest path </b>
+        between nodes in a graph. It can be used in <b>weighted</b> and
+        <b> unweighted</b> graphs. Using <b> negatively weighted</b> edges
+        wont't guarantees shortest path. Weighted nodes not implemented in this
+        visualization example.
+      </span>
+    );
+  };
+
+  /**
+   * return description
+   * @param algorithm algorithm
+   */
+  const getDescription = (algorithm: enums.Algorithm) => {
+    let description: JSX.Element = <React.Fragment />;
+    switch (algorithm) {
+      case enums.Algorithm.Dijkstra:
+        description = getDijkstraDescription();
+        break;
+    }
+
+    return description;
+  };
+
   return (
     <div className="stage-footer">
-      <span className="note">Note:- </span>
-      <span>Algorithm description goes here</span>
+      <span className="note">Note: </span>
+      {getDescription(props.selectedAlgorithm)}
     </div>
   );
 };
