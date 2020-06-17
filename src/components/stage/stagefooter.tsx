f01d9b8e560666dc54f4d94b89ef86bbf133d38a
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-06-17 06:54:09 +0530

    Added description for algorithms

--- src/components/stage/stagefooter.tsx
+++ src/components/stage/stagefooter.tsx
@@ -18,6 +18,43 @@ const StageFooter: React.FunctionComponent<Props> = (props) => {
     );
   };
 
+  const getBfsDescription = () => {
+    return (
+      <span>
+        Breadth-first search (BFS) is a traversing algorithm where traversing
+        starts from a selected node (<b>starting node</b>) and traverse the
+        graph <b>layerwise</b> thus exploring the neighbour nodes. BFS won't
+        work on <b>weighted</b> graps. It <b>guarantees shortest path</b> on
+        unweighted graphs.
+      </span>
+    );
+  };
+
+  const getDfsDescription = () => {
+    return (
+      <span>
+        Depth-first search (DFS) is an algorithm for traversing tree or graph
+        data structures. The algorithm starts at the <b>root node</b> and
+        explores as far as possible along each branch before backtracking. DFS
+        won't work on <b>weighted</b> graps, and <b>won't guarantees </b>
+        shortest path.
+      </span>
+    );
+  };
+
+  const getAStarDescription = () => {
+    return (
+      <span>
+        A* ('A-star') is a graph traversal and path search algorithm, which is
+        often used in many fields of computer science due to its completeness,
+        optimality, and optimal efficiency. A* achieves better performance by
+        using <b>heuristics</b> to guide its search. A-star work on both
+        <b> weighted</b> and <b>unweighted</b> graps. This algorithm guarantees{" "}
+        <b>shortest path</b>.
+      </span>
+    );
+  };
+
   /**
    * return description
    * @param algorithm algorithm
@@ -27,6 +64,15 @@ const StageFooter: React.FunctionComponent<Props> = (props) => {
     switch (algorithm) {
       case enums.Algorithm.Dijkstra:
         description = getDijkstraDescription();
+        break;
+      case enums.Algorithm.Bfs:
+        description = getBfsDescription();
+        break;
+      case enums.Algorithm.Dfs:
+        description = getDfsDescription();
+        break;
+      case enums.Algorithm.AStar:
+        description = getAStarDescription();
         break;
     }
 
